f789716483b0c4f2477f6db834bdce03cdf06bef
Mohamadreza Azizi <154751813+rezalwl@users.noreply.github.com>
2024-08-24 18:53:22 +0330

    create initial template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Birthdate from "./components/Birthdate";
+import "./App.css";
+
+
+function App() {
+  return (
+    <>
+      <Birthdate />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Birthdate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Birthdate = () => {
+  return (
+    <div>Birthdate</div>
+  )
+}
+
+export default Birthdate
